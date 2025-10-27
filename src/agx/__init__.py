c5efb489d56984596c0a91a892d5962c624ccf0c
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-10-27 22:47:18 +0000

    Add node class.

--- src/agx/__init__.py
+++ src/agx/__init__.py
@@ -4,24 +4,25 @@ from agx import utilities
 from agx._internal.building_block_enum import (
     BuildingBlockConfiguration,
     get_custom_bb_configurations,
-    get_potential_bb_dicts,
 )
 from agx._internal.comparisons import (
     get_bb_topology_code_graph,
     passes_graph_bb_iso,
 )
 from agx._internal.enumeration import TopologyIterator
+from agx._internal.node import Node, NodeType
 from agx._internal.topology_code import TopologyCode
 
 __all__ = [
     "BuildingBlockConfiguration",
     "Constructed",
+    "Node",
+    "NodeType",
     "TopologyCode",
     "TopologyIterator",
     "generate_graph_type",
     "get_bb_topology_code_graph",
     "get_custom_bb_configurations",
-    "get_potential_bb_dicts",
     "get_regraphed_molecule",
     "get_stk_topology_code",
     "get_vertexset_molecule",
