6fb7d4986cd2a6143f04c166f61031f501135075
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-10-28 22:31:56 +0000

    Add node container.

--- src/agx/_internal/node.py
+++ src/agx/_internal/node.py
@@ -8,14 +8,37 @@ logger = logging.getLogger(__name__)
 
 @dataclass(eq=True, frozen=True)
 class Node:
-    """Container for a node."""
+    """Container for a node.
+
+    Parameters:
+        id:
+            The id of the node.
+
+        type_id:
+            This defines a collection of nodes as equivalent.
+
+        num_connections:
+            The number of connections the node makes.
+
+    """
 
     id: int
+    type_id: int
     num_connections: int
 
 
 @dataclass(eq=True, frozen=True)
 class NodeType:
-    """Container for a node types."""
+    """Container for a node types.
 
+    Parameters:
+        type_id:
+            This defines a collection of nodes as equivalent.
+
+        num_connections:
+            The number of connections the node makes.
+
+    """
+
+    type_id: int
     num_connections: int
