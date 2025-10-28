bff36a6a18cea7e69deb90741732525ccd34bbb5
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-10-28 22:34:16 +0000

    Update interface.

--- src/agx/__init__.py
+++ src/agx/__init__.py
@@ -1,37 +1,19 @@
 """agx package."""
 
 from agx import utilities
-from agx._internal.building_block_enum import (
-    BuildingBlockConfiguration,
-    get_custom_bb_configurations,
-)
-from agx._internal.comparisons import (
-    get_bb_topology_code_graph,
-    passes_graph_bb_iso,
-)
+from agx._internal.configuration import Configuration
+from agx._internal.configuredcode import ConfiguredCode
 from agx._internal.enumeration import TopologyIterator
 from agx._internal.node import Node, NodeType
 from agx._internal.topology_code import TopologyCode
 
 __all__ = [
-    "BuildingBlockConfiguration",
+    "Configuration",
+    "ConfiguredCode",
     "Constructed",
     "Node",
     "NodeType",
     "TopologyCode",
     "TopologyIterator",
-    "generate_graph_type",
-    "get_bb_topology_code_graph",
-    "get_custom_bb_configurations",
-    "get_regraphed_molecule",
-    "get_stk_topology_code",
-    "get_vertexset_molecule",
-    "graph_optimise_cage",
-    "optimise_cage",
-    "optimise_from_files",
-    "passes_graph_bb_iso",
-    "points_on_sphere",
-    "target_optimisation",
-    "try_except_construction",
     "utilities",
 ]
