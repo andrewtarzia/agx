58a67628520110757961249001ffb6b7ffe2bcfa
Andrew Tarzia <andrew.tarzia@gmail.com>
2026-03-11 11:26:55 +0000

    Make exception accessible.

--- src/agx/__init__.py
+++ src/agx/__init__.py
@@ -3,11 +3,12 @@
 from agx import utilities
 from agx._internal.configuration import Configuration
 from agx._internal.configuredcode import ConfiguredCode
-from agx._internal.enumeration import TopologyIterator
+from agx._internal.enumeration import BuildNotPossibleError, TopologyIterator
 from agx._internal.node import Node, NodeType
 from agx._internal.topology_code import TopologyCode
 
 __all__ = [
+    "BuildNotPossibleError",
     "Configuration",
     "ConfiguredCode",
     "Constructed",
